e39f017d46c09581fd03b9cf8cd12d594d06ee62
Siryi-Oleksandr <siryyoleksandr@gmail.com>
2023-03-11 19:07:10 +0200

    base settings

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import { GlobalStyle } from 'GlobalStyle';
+import { Container } from './App.styled';
+
+const App = () => {
+  return (
+    <Container>
+      <p>React homework template</p>
+      <GlobalStyle />
+    </Container>
+  );
+};
+
+export default App;
---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  gap: 15px;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 15px;
+  width: 980px;
+  border-radius: 8px;
+
+  background-color: #4fc6cab9;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+`;
